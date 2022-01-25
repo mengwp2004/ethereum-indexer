e84b2317b8624097de01c8025a5993c2c4f0e6f9
mengwp2004 <mengwp_2012@163.com>
2022-01-25 10:52:10 +0800

    listen config
    Signed-off-by: mengwp2004 <mengwp_2012@163.com>

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/configuration/NftListenerConfiguration.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/configuration/NftListenerConfiguration.kt
@@ -15,12 +15,21 @@ import com.rarible.protocol.nft.core.model.ReduceSkipTokens
 import com.rarible.protocol.nft.core.producer.BatchedConsumerWorker
 import com.rarible.protocol.nft.core.service.item.meta.InternalItemHandler
 import com.rarible.protocol.nft.core.service.token.meta.InternalCollectionHandler
+import io.daonomic.rpc.mono.WebClientTransport
 import io.micrometer.core.instrument.MeterRegistry
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import scalether.core.EthPubSub
+import scalether.core.MonoEthereum
+import scalether.core.PubSubTransport
+import scalether.domain.Address
+import scalether.transaction.MonoTransactionPoller
+import scalether.transaction.ReadOnlyMonoTransactionSender
+import scalether.transport.WebSocketPubSubTransport
 
 @EnableMongock
 @EnableRaribleRedisLock
@@ -86,4 +95,28 @@ class NftListenerConfiguration(
     @Bean
     fun collectionMetaExtenderWorkerStarter(collectionMetaExtenderWorker: BatchedConsumerWorker<NftItemEventDto>): CommandLineRunner =
         CommandLineRunner { collectionMetaExtenderWorker.start() }
+
+    @Bean
+    fun testEthereum(@Value("\${parityUrls}") url: String): MonoEthereum {
+        return MonoEthereum(WebClientTransport(url, MonoEthereum.mapper(), 10000, 10000))
+    }
+
+    @Bean
+    fun testSender(ethereum: MonoEthereum) = ReadOnlyMonoTransactionSender(ethereum, Address.ONE())
+
+    @Bean
+    fun poller(ethereum: MonoEthereum): MonoTransactionPoller {
+        return MonoTransactionPoller(ethereum)
+    }
+
+    @Bean
+    fun pubSubTransport(@Value("\${parityUrls}") url: String): WebSocketPubSubTransport {
+        return WebSocketPubSubTransport(url, Int.MAX_VALUE)
+    }
+
+    @Bean
+    fun ethPubSub(transport: PubSubTransport): EthPubSub {
+        return EthPubSub(transport)
+    }
+
 }
